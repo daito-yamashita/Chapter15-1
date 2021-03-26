42206441cf0b3b599eccb71afc2e2ab1108c90a8
ns-yamashita <daito.yamashita@na-s.jp>
2021-03-26 15:50:38 +0900

    タップをするとアザラシが移動します

--- Chapter15-1/ViewController.swift
+++ Chapter15-1/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var azarashi: UIImageView!
+    
+    // ビューがタップされたら実行
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        
+        // タップされた座標を調べる
+        let tapPoint = sender.location(in: view)
+        
+        // 指定した最終値になるようにアニメーションする
+        UIView.animate(
+            
+            // 1秒間で再生
+            withDuration: 1.0,
+            
+            // 待ち時間なし
+            delay: 0,
+            
+            // イーズインアウト
+            options: [.curveEaseInOut],
+            
+            // タップした座標に移動
+            animations: {
+                self.azarashi.center = tapPoint
+            },
+            
+            completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let azarashi2 = UIImageView(image: UIImage(named: "azarashi"))
+        azarashi2.center = CGPoint(x: 50, y: 500)
+        view.addSubview(azarashi2)
+        
+        UIView.animate(
+            withDuration: 1.0,
+            delay: 0,
+            options: [
+                .curveEaseInOut,
+                .autoreverse,
+                .repeat
+            ],
+            animations: {
+                azarashi2.center.x = self.view.frame.width - 50
+            },
+            completion: nil)
     }
-
-
 }
 
